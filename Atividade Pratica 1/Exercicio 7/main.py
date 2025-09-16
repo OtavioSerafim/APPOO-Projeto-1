ab76912674b0b72da85e0dcca832bc85115d0cd7
OtavioSerafim <otavio.serafim2002@gmail.com>
2025-09-16 20:17:03 -0300

    remove comentários do código e ativa chamadas de métodos para cálculo de área e perímetro

--- Atividade Pratica 1/Exercicio 7/main.py
+++ Atividade Pratica 1/Exercicio 7/main.py
@@ -3,14 +3,14 @@ from Retangulo import Retangulo
 from Triangulo import Triangulo
 
 def main():
-    # r = Retangulo()
-    # q = Quadrado()
+    r = Retangulo()
+    q = Quadrado()
     t = Triangulo()
 
-    # print(r.calcular_area())
-    # print(r.calcular_perimetro())
-    # print(q.calcular_area())
-    # print(q.calcular_perimetro())
+    print(r.calcular_area())
+    print(r.calcular_perimetro())
+    print(q.calcular_area())
+    print(q.calcular_perimetro())
     print(t.calcular_area())
     print(t.calcular_perimetro())
     print(t.triangulo_retangulo())
