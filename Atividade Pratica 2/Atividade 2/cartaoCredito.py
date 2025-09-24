01018e0e89233f95c21cd6013f212b994ea9340e
OtavioSerafim <otavio.serafim2002@gmail.com>
2025-09-24 08:57:34 -0300

    Adiciona as classes CartaoCredito, Trasnferencia Bancaria e Paypal s implementação do método salvar_pagamento
    Co-authored-by: Germano Fagundes <germano-fagundes@users.noreply.github.com>

---
+++ Atividade Pratica 2/Atividade 2/cartaoCredito.py
@@ -0,0 +1,14 @@
+from metodoPagamento import MetodoPagamento
+from datetime import date
+
+class CartaoCredito(MetodoPagamento):
+    def __init__(self, valor, data):
+        super().__init__(valor, data)
+
+    def salvar_pagamento(self, valor):
+        with open("pagamentos.txt", "a") as f:
+            f.write(f"{self.data} - Pagamento de valor {valor:.2f} processado com cartão de crédito\n")
+
+    def processar_pagamento(self):
+        self.salvar_pagamento(self.valor)
+        print(f"O Pagamento foi processado com cartão de crédito no valor de {self.valor} na data {self.data}.")
---
+++ Atividade Pratica 2/Atividade 2/metodoPagamento.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from datetime import date
+class MetodoPagamento(ABC):
+    def __init__(self, valor, data=date.today()):
+        try:
+            if not type(valor) in (float, int) or isinstance(valor, bool):
+                raise ValueError("Valor deve ser um número (int ou float)")
+            if valor <= 0:
+                raise ValueError("Valor deve ser positivo")
+            if not isinstance(data, date):
+                raise ValueError("Data inválida")
+            self.valor = valor
+            self.data = data
+        except ValueError as e:
+            print(f"Erro ao criar método de pagamento: {e}")
+        except:
+            print("Erro não esperado na criação da instância!")
+            
+
+    @abstractmethod
+    def processar_pagamento(self):
+        pass
+
+    @abstractmethod
+    def salvar_pagamento(self, valor):
+        pass
